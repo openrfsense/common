36126b3aa39afe74662487aae2831b1db25971bf
Baldomo <leobaldin.2000@gmail.com>
2022-03-21 23:04:36 +0100

    Added Retriever and documentation to keystore, fixed small possible bugs

--- keystore/key.go
+++ keystore/key.go
@@ -1,13 +1,44 @@
+// Copyright (C) 2022 OpenRFSense
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package keystore
 
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 var ErrNoKeyFound = errors.New("no key found in keystore")
 
-var keyMap = make(map[string]map[string]string)
+var (
+	keyRetriever Retriever
+
+	keyMap = make(map[string]map[string]string)
+)
+
+// The key retriever function will be passed the requested channel name and access string.
+// It is expected to return the correct key for the given parameters. An example implementation
+// of a key retriever would consist in a function which requests a key using a secret from
+// a Emitter broker, or a request to a web API.
+type Retriever func(string, string) string
+
+// Initializes the internal keystore and sets a Retriever function.
+func Init(retriever Retriever) {
+	keyRetriever = retriever
+}
 
 // Arbitrarily set a key in the keystore.
 func Set(channel string, access string, newKey string) {
@@ -18,17 +49,19 @@ func Set(channel string, access string, newKey string) {
 }
 
 // Tries retrieving a key from the keystore. If a key for the specified channel and access mode is not found,
-// an error wrapping ErrNoKeyFound is returned.
+// and the retriever also returns an empty string, an error wrapping ErrNoKeyFound is returned.
 func Must(channel string, access string) (string, error) {
 	if acc, ok := keyMap[channel]; ok {
 		if key, kOk := acc[access]; kOk {
 			return key, nil
 		}
-
-		return "", fmt.Errorf("%w: no key registered for channel %s and access %s", ErrNoKeyFound, channel, access)
 	}
 
-	return "", fmt.Errorf("%w: keystore does not contain channel %s", ErrNoKeyFound, channel)
+	key := keyRetriever(channel, access)
+	if strings.TrimSpace(key) == "" {
+		return "", fmt.Errorf("%w: no key registered for channel %s and access %s", ErrNoKeyFound, channel, access)
+	}
+	return key, nil
 }
 
 // Tries retrieving a key from the keystore. If a key for the specified channel and access mode is not found,
@@ -42,7 +75,7 @@ func Get(channel string, access string) string {
 		keyMap[channel] = make(map[string]string)
 	}
 
-	key := ""
-	keyMap[channel][access] = key
+	key := keyRetriever(channel, access)
+	Set(channel, access, key)
 	return key
 }
