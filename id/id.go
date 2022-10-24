6b9ac1216d219ad10c19e9ddf12115f402eeca75
Baldomo <leobaldin.2000@gmail.com>
2022-10-24 22:45:07 +0200

    Added ID generation with package id

---
+++ id/id.go
@@ -0,0 +1,38 @@
+package id
+
+import (
+	"math/rand"
+	"unsafe"
+)
+
+// Generates an arbitrarily long random string using an byte array as the seed.
+// Only lowercase modern English alphabet are used.
+func GenerateFromString(seed []byte, length int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+	const (
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	)
+
+	intSeed := int64(0)
+	for _, b := range seed {
+		intSeed += int64(b)
+	}
+
+	src := rand.NewSource(intSeed)
+	b := make([]byte, length)
+	for i, cache, remain := length-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return *(*string)(unsafe.Pointer(&b))
+}
