0948c4eb6e8e00724cc211ba2e8acbdc49493756
Baldomo <leobaldin.2000@gmail.com>
2022-10-24 23:00:35 +0200

    Refactored id.go, added seedless (using time) ID generation

--- id/id.go
+++ id/id.go
@@ -2,12 +2,32 @@ package id
 
 import (
 	"math/rand"
+	"time"
 	"unsafe"
 )
 
+// Generates and arbitrarily long random string using the current time as the seed.
+func Generate(length int) string {
+	src := rand.NewSource(time.Now().Unix())
+
+	return generateWithSource(src, length)
+}
+
 // Generates an arbitrarily long random string using an byte array as the seed.
 // Only lowercase modern English alphabet are used.
-func Generate(seed []byte, length int) string {
+func GenerateFromBytes(seed []byte, length int) string {
+	intSeed := int64(0)
+	for _, b := range seed {
+		intSeed += int64(b)
+	}
+	src := rand.NewSource(intSeed)
+
+	return generateWithSource(src, length)
+}
+
+// Generates a random string given its length and a generator function returning
+// a number (int64) on call.
+func generateWithSource(src rand.Source, length int) string {
 	const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 	const (
 		letterIdxBits = 6                    // 6 bits to represent a letter index
@@ -15,12 +35,6 @@ func Generate(seed []byte, length int) string {
 		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 	)
 
-	intSeed := int64(0)
-	for _, b := range seed {
-		intSeed += int64(b)
-	}
-
-	src := rand.NewSource(intSeed)
 	b := make([]byte, length)
 	for i, cache, remain := length-1, src.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
